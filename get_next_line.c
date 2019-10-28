5e1d403f85cc04e1e981ff6c08bcc14a882260d3
Database Nigella <dnigella@ga-a1.21-school.ru>
2019-10-28 03:52:12 +0300

    maybembmbmb

--- get_next_line.c
+++ get_next_line.c
@@ -1,40 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   gnl.c                                              :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dnigella <dnigella@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 17:22:02 by dnigella          #+#    #+#             */
-/*   Updated: 2019/10/28 03:24:29 by dnigella         ###   ########.fr       */
+/*   Updated: 2019/10/28 03:48:39 by dnigella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int			azaza(char **qwe, char **line)
+static int			azaza(char **qwe, char **line, int fd)
 {
 	size_t			i;
 	char			*tmp;
 
 	i = 0;
-	while (qwe[i] != '\n' && qwe[i] != '\0')
+	while (qwe[fd][i] != '\n' && qwe[fd][i] != '\0')
 		i++;
-	if (qwe[i] == '\0')
+	if (qwe[fd][i] == '\0')
 	{
-		*line = ft_strdup(qwe[i]);
-		free(qwe);
+		*line = ft_strdup(qwe[fd]);
+		free(qwe[fd]);
 		return (1);
 	}
-	if (qwe[i] == '\n')
+	if (qwe[fd][i] == '\n')
 	{
-		*line = ft_strsub(qwe, 0, i);
-		tmp = ft_strsub(qwe, i + 1, ft_strlen(qwe) - i);
-		free(qwe);
-		*qwe = ft_strdup(tmp);
+		*line = ft_strsub(qwe[fd], 0, i);
+		tmp = ft_strsub(qwe[fd], i + 1, ft_strlen(qwe[fd]) - i);
+		free(qwe[fd]);
+		qwe[fd] = ft_strdup(tmp);
 		free(tmp);
-		return (1);
 	}
+	else
+	{
+		*line = ft_strdup(qwe[fd]);
+		free(qwe[fd]);
+	}
+	return (1);
 }
 
 int					get_next_line(const int fd, char **line)
@@ -57,10 +62,9 @@ int					get_next_line(const int fd, char **line)
 		if (ft_strchr(qwe[fd], '\n'))
 			break ;
 	}
-	free(tmp);
 	if (ret < 0)
 		return (-1);
 	if (ret == 0 && (qwe[fd][0] == '\0' || (!qwe[fd])))
 		return (0);
-	return (azaza(qwe[fd], line));
+	return (azaza(qwe, line, fd));
 }
